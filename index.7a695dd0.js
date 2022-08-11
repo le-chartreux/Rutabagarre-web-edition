613b0811a2bb1a68e15dca35a8f2e7a52c437efe
le-chartreux <le-chartreux-vert@protonmail.com>
2022-08-11 21:29:42 +0200

    Updates

--- index.7a695dd0.js
+++
@@ -1,2 +0,0 @@
-class t{constructor(t,n){this.height=t,this.width=n}}let n=document.getElementById("start-button");n?n.onclick=function(){alert("Game started"),new t(13,200)}:console.error("Start button not found on DOM, so we can't bind it.");
-//# sourceMappingURL=index.7a695dd0.js.map
