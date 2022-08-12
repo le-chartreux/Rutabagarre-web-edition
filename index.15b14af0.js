fcb8a8c3064dcf55eb9c03dd2bd36f2685edb496
le-chartreux <le-chartreux-vert@protonmail.com>
2022-08-13 00:44:32 +0200

    Updates

--- index.15b14af0.js
+++
@@ -1,2 +0,0 @@
-let t=new class{},e=new class{};new class{constructor(t,e){this.model=t,this.view=e}setup(){let t=document.getElementById("start-button");t?t.onclick=this.start:console.error("Start button not found on DOM, so we can't bind it.")}start(){alert("start button clicked")}}(t,e).setup();
-//# sourceMappingURL=index.15b14af0.js.map
