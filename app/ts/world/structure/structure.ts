edd253336ba39638246825ff2cc045064d010a6c
le-chartreux <le-chartreux-vert@protonmail.com>
2022-08-17 00:28:03 +0200

    debugging memory leak

--- app/ts/world/structure/structure.ts
+++ app/ts/world/structure/structure.ts
@@ -4,18 +4,22 @@ import * as THREE from "three";
 
 class Structure extends PhysicalElement {
 
+    private cube: any = null
+    // geometry = vertices
+    private readonly geometry = new THREE.BoxGeometry(1, 1, 1)
+    // material = how the shape will look in terms of colors & textures; here orange
+    private readonly material = new THREE.MeshStandardMaterial({color: 0xFFAD00})
+
     public get object3D(): THREE.Object3D {
         // for now, it only creates a cube
+        if (this.cube == null) {
+            this.cube = new THREE.Mesh(this.geometry, this.material)
+        }
+        this.cube.position.x = this.x
+        this.cube.position.y = this.y
+        this.cube.position.z = 0
 
-        // geometry = vertices
-        const geometry = new THREE.BoxGeometry()
-        // material = how the shape will look in terms of colors & textures; here orange
-        const material = new THREE.MeshStandardMaterial({color: 0xFFAD00})
-        const cube = new THREE.Mesh(geometry, material)
-        cube.position.x = this.x
-        cube.position.y = this.y
-        cube.position.z = -5
-        return cube
+        return this.cube
     }
 
 }
